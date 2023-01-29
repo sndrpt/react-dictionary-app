b3d35aead813ba0083bf11a53801c34adc5c0dc9
sndrpt <103604366+sndrpt@users.noreply.github.com>
2023-01-29 12:02:12 +0100

    Create repository and basic structure

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header container">
+        <h1>Dictionary</h1>
+        <input
+          type="search"
+          placeholder="Search..."
+          autoFocus="on"
+          className="form-control"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
